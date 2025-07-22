62488d4f5f71c538926f305f77570e80a0b9defb
Sabarish <sabarish1422@gmail.com>
2025-07-22 12:19:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// smooth scroll to sections
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute("href"));
+    if (target) {
+      target.scrollIntoView({ behavior: "smooth" });
+    }
+  });
+});
